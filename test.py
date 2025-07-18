9c0d6f6a9408044a4824492a6ae641b3056f0a60
DMegatron <soujatyabhunia2003@gmail.com>
2025-07-18 11:23:33 +0530

    change 1

--- test.py
+++ test.py
@@ -1,13 +1,11 @@
-from flask import Flask, request, render_template, send_from_directory
-import os
+from flask import Flask, request, render_template
 from datetime import datetime
 
 app = Flask(__name__)
-UPLOAD_FOLDER = 'static/uploads'
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 latest_data = {
     'filename': None,
+    'image_bytes': None,
     'text': ''
 }
 
@@ -21,17 +19,11 @@ def upload():
     text = request.form.get('text')
 
     if image:
-        filename = datetime.now().strftime("%Y%m%d_%H%M%S.jpg")
-        filepath = os.path.join(UPLOAD_FOLDER, filename)
-        image.save(filepath)
-        latest_data['filename'] = filename
+        latest_data['filename'] = datetime.now().strftime("%Y%m%d_%H%M%S.jpg")
+        latest_data['image_bytes'] = image.read()  # Store image in memory (bytes)
         latest_data['text'] = text
 
     return 'OK'
-
-@app.route('/static/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(UPLOAD_FOLDER, filename)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
